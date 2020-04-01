4bd74f5c96d98b80794ca422322e36a0e5e76573
root <root@devx-python001.ziyotek.local>
2020-04-01 00:16:38 -0400

    added new changes

---
+++ conditionals.py
@@ -0,0 +1,9 @@
+likes_pineapple_on_pizza = "NO"
+
+if(likes_pineapple_on_pizza == "Absolutely!"):
+        print("You are wierd!")
+elif(likes_pineapple_on_pizza == "Sort of"):
+    print("I guess you're ok")
+else: 
+    print("you are completely normal")
+
